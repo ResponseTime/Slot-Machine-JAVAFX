fa18006f295b33e042096a344f2aed10029ecc13
Looper <aayush.bhattacharjee2002@gmail.com>
2021-06-27 13:03:32 +0530

    Removed unused imports

--- slot/controller.java
+++ slot/controller.java
@@ -1,14 +1,12 @@
 package slot;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
+
 
 import java.util.Random;
 
 import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
+
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
